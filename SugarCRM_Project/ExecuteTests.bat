c62f2e55894bf705b7e915204d1cbd1dc6840f0c
cptestingqa <cptestingqa@gmail.com>
2016-02-03 11:33:43 +0530

    Added Project

---
+++ SugarCRM_Project/ExecuteTests.bat
@@ -0,0 +1,15 @@
+set SOURCE=%~dp0
+
+SET today=%Date:~10,4%%Date:~4,2%%Date:~7,2%
+set t=%time:~0,8%
+set t=%t::=%
+set t=%t: =0%
+set timestamp=%today%_%t%
+
+
+set projDir=D:\Selenium\automationtests-master\SugarCRM_RobotFramework
+
+cd %projDir%
+D:
+
+call pybot --name SampleReport --variable BROWSER:gc --outputdir D:\TestReports\FFTestRun01 --logtitle SampleJenkingsJobLog --reporttitle SampleJenkingsJobTestReports "Test Modules\Gmail.txt"
